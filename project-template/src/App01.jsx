78790952c1fc31c65d7139ec1fe3cacd36cd5789
arushiahmed <aaahmed@umass.edu>
2019-03-25 23:14:02 -0400

    Updated App01.jsx
    Added State which is the sign up button

--- project-template/src/App01.jsx
+++ project-template/src/App01.jsx
@@ -1,67 +1,63 @@
-// This is a place holder for the initial application state.
-const state = [
+let Parent  = React.createClass({
+  getInitialState:function(){
+    return {signup:false,login:true}
+  },
+    switch:function(word)
+    {
+    let signup,login;
+    if(word == "signup"){signup = true;login = false;}
+    else{login = true; signup = false;}
+    return this.setState({login:login,signup:signup})
+    },
+    
+    render:function(){
+            return (
+              <div>
+                    <div id="buttons">
+                      <p id="signupButton" onClick={this.switch.bind(null,"signup")} className={this.state.signup}>Sign In</p>
+                      <p id="loginButton" onClick={this.switch.bind(null,"login")} className={this.state.login}> Login</p>
+                     </div>
+                      {this.state.signup?<Signup/>:null}
+                      {this.state.login?<Login />:null}
+                </div>
+              )
+            }
+          })
 
-];
-
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById('contents');
-var contentNode2 = document.getElementById('login');
-var contentNode3 = document.getElementById('signup');
-
-class MyComponent extends React.Component {
-  constructor() {
-    super();
+class Signup extends React.Component {    
+     constructor() {
+      super();
   }
-
-  render() {
-    return (
-      <div>
-        <h1>Login In</h1>
-      </div>
-    );
-  }
-}
+            render(){
+            return (
+                <div>
+                   <div id="signup">
+                   <input type="text" id="name" placeholder="username"/>
+                    <input type="email" id="email" placeholder="Email"/>
+                    <input type="password" id="password" placeholder="Password"/>
+                    <input type="password" id="confirm" placeholder="Confirm Password"/>
+                    <button id="send"><a href="/view02.html">Sign Up</a></button>
+                </div>
+              </div>
+            )
+          }
+        }
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-    username:'',
-    password:' '
-    };
+       constructor() {
+       super();
   }
+              render(){
+              return (
+                    <div>
+                      <div id="login">
+                      <input type="email" id="email" placeholder="Email"/>
+                      <input type="password" id="password" placeholder="Password"/>
+                      <button id="send"><a href="/view02.html">Login</a></button>
+                      </div>
+                    </div>
+                  )
+                }
+              }
 
-  render() {
-    return (
-    <form>
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" onChange = {(event,newValue) => this.setState({username:newValue})}/>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw"  onChange = {(event,newValue) => this.setState({password:newValue})}/>
-        
-      </form>
-    );
-  }
-}
-
-class SignUp extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={(event) => this.handleClick(event)}><a href="/view02.html">Login</a></button>
-        <button onClick={(event) => this.handleClick(event)}><a href="/view04.html">Sign Up</a></button>
-      </div>
-    );
-  }
-}
-
-// This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
-ReactDOM.render(<Login />, contentNode2);
-ReactDOM.render(<SignUp />, contentNode3);
-
+ReactDOM.render(<Parent/>,document.getElementById("contents"))
--- project-template/static/App01.js
+++ project-template/static/App01.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,88 +8,116 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// This is a place holder for the initial application state.
-var state = [];
+var Parent = React.createClass({
+  displayName: "Parent",
 
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById('contents');
-var contentNode2 = document.getElementById('login');
-var contentNode3 = document.getElementById('signup');
+  getInitialState: function getInitialState() {
+    return { signup: false, login: true };
+  },
+  switch: function _switch(word) {
+    var signup = void 0,
+        login = void 0;
+    if (word == "signup") {
+      signup = true;login = false;
+    } else {
+      login = true;signup = false;
+    }
+    return this.setState({ login: login, signup: signup });
+  },
 
-var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "div",
+        { id: "buttons" },
+        React.createElement(
+          "p",
+          { id: "signupButton", onClick: this.switch.bind(null, "signup"), className: this.state.signup },
+          "Sign In"
+        ),
+        React.createElement(
+          "p",
+          { id: "loginButton", onClick: this.switch.bind(null, "login"), className: this.state.login },
+          " Login"
+        )
+      ),
+      this.state.signup ? React.createElement(Signup, null) : null,
+      this.state.login ? React.createElement(Login, null) : null
+    );
+  }
+});
 
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
+var Signup = function (_React$Component) {
+  _inherits(Signup, _React$Component);
 
-    return _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
+  function Signup() {
+    _classCallCheck(this, Signup);
+
+    return _possibleConstructorReturn(this, (Signup.__proto__ || Object.getPrototypeOf(Signup)).call(this));
   }
 
-  _createClass(MyComponent, [{
-    key: 'render',
+  _createClass(Signup, [{
+    key: "render",
     value: function render() {
       return React.createElement(
-        'div',
+        "div",
         null,
         React.createElement(
-          'h1',
-          null,
-          'Login In'
+          "div",
+          { id: "signup" },
+          React.createElement("input", { type: "text", id: "name", placeholder: "username" }),
+          React.createElement("input", { type: "email", id: "email", placeholder: "Email" }),
+          React.createElement("input", { type: "password", id: "password", placeholder: "Password" }),
+          React.createElement("input", { type: "password", id: "confirm", placeholder: "Confirm Password" }),
+          React.createElement(
+            "button",
+            { id: "send" },
+            React.createElement(
+              "a",
+              { href: "/view02.html" },
+              "Sign Up"
+            )
+          )
         )
       );
     }
   }]);
 
-  return MyComponent;
+  return Signup;
 }(React.Component);
 
 var Login = function (_React$Component2) {
   _inherits(Login, _React$Component2);
 
-  function Login(props) {
+  function Login() {
     _classCallCheck(this, Login);
 
-    var _this2 = _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this, props));
-
-    _this2.state = {
-      username: '',
-      password: ' '
-    };
-    return _this2;
+    return _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this));
   }
 
   _createClass(Login, [{
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _this3 = this;
-
       return React.createElement(
-        'form',
+        "div",
         null,
         React.createElement(
-          'label',
-          { 'for': 'uname' },
+          "div",
+          { id: "login" },
+          React.createElement("input", { type: "email", id: "email", placeholder: "Email" }),
+          React.createElement("input", { type: "password", id: "password", placeholder: "Password" }),
           React.createElement(
-            'b',
-            null,
-            'Username'
+            "button",
+            { id: "send" },
+            React.createElement(
+              "a",
+              { href: "/view02.html" },
+              "Login"
+            )
           )
-        ),
-        React.createElement('input', { type: 'text', placeholder: 'Enter Username', name: 'uname', onChange: function onChange(event, newValue) {
-            return _this3.setState({ username: newValue });
-          } }),
-        React.createElement(
-          'label',
-          { 'for': 'psw' },
-          React.createElement(
-            'b',
-            null,
-            'Password'
-          )
-        ),
-        React.createElement('input', { type: 'password', placeholder: 'Enter Password', name: 'psw', onChange: function onChange(event, newValue) {
-            return _this3.setState({ password: newValue });
-          } })
+        )
       );
     }
   }]);
@@ -97,55 +125,4 @@ var Login = function (_React$Component2) {
   return Login;
 }(React.Component);
 
-var SignUp = function (_React$Component3) {
-  _inherits(SignUp, _React$Component3);
-
-  function SignUp() {
-    _classCallCheck(this, SignUp);
-
-    return _possibleConstructorReturn(this, (SignUp.__proto__ || Object.getPrototypeOf(SignUp)).call(this));
-  }
-
-  _createClass(SignUp, [{
-    key: 'render',
-    value: function render() {
-      var _this5 = this;
-
-      return React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'button',
-          { onClick: function onClick(event) {
-              return _this5.handleClick(event);
-            } },
-          React.createElement(
-            'a',
-            { href: '/view02.html' },
-            'Login'
-          )
-        ),
-        React.createElement(
-          'button',
-          { onClick: function onClick(event) {
-              return _this5.handleClick(event);
-            } },
-          React.createElement(
-            'a',
-            { href: '/view04.html' },
-            'Sign Up'
-          )
-        )
-      );
-    }
-  }]);
-
-  return SignUp;
-}(React.Component);
-
-// This renders the JSX component inside the content node:
-
-
-ReactDOM.render(React.createElement(MyComponent, null), contentNode);
-ReactDOM.render(React.createElement(Login, null), contentNode2);
-ReactDOM.render(React.createElement(SignUp, null), contentNode3);
+ReactDOM.render(React.createElement(Parent, null), document.getElementById("contents"));
